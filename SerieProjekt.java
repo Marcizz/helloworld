247eeb888d9e888f8d6b21bca7acc5a0f282de61
Marcus Laitala <marcus.laitala@gmail.com>
2019-01-14 10:57:26 +0100

    Skapat Main för SerieProjekt

---
+++ SerieProjekt.java
@@ -0,0 +1,10 @@
+import java.net.URL; /**
+ *
+ * @author Marcus Laitala
+ */ public class SerieProjekt {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
